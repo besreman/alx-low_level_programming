c28e08cc5f1c622fcba4c3c8b0b324e215382edc
besreman <besratmesele@gmail.com>
2022-04-09 17:35:04 +0000

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        puts("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
